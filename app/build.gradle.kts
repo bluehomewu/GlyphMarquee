bcdc075d4aa464da2c14b4901f1a750ceb924966
EdwardWu <bluehome.wu@gmail.com>
2026-04-06 22:16:54 +0800

    chore: bump version to v1.2.0 (versionCode 4)
    versionCode: 3 -> 4
    versionName: 1.1.1 -> 1.2.0
    
    Version naming convention: x.y.z
      x, y = official release numbers
      z    = test/pre-release build number
    
    Signed-off-by: EdwardWu <bluehome.wu@gmail.com>

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,8 +13,8 @@ android {
         applicationId = "tw.bluehomewu.glyphmarquee"
         minSdk = 34
         targetSdk = 36
-        versionCode = 3
-        versionName = "1.1.1"
+        versionCode = 4
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
